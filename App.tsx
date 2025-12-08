151735afa3735c2b37467ab6ce631d71f03f96a8
Sanjay17u <sanjay.solanki.17.om@gmail.com>
2025-12-08 10:38:39 +0530

    W1D2P2 - Complete

--- App.tsx
+++ App.tsx
@@ -1,45 +1,107 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+
+  const [text, setText] = useState('')
+  const [taskLists, setTaskLists] = useState<string[]>([])
+
+  const TextHandler = (e: any) => {
+    setText(e)
+  }
+
+  const AddTaskHandler = () => {
+    if (text === '') {
+      return;
+    } else {
+      setTaskLists([...taskLists, text])
+      setText('')
+    }
+  }
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+    <>
+      <View style={styles.container}>
+        <View style={styles.InputContainer}>
+          <Text style={styles.inputTitle}>Todo List (Add Items)</Text>
+          <View style={styles.AddInput}>
+            <TextInput placeholder='Add Task' value={ text } onChangeText={ TextHandler } style={styles.mainInput} />
+            <Button onPress={ AddTaskHandler } title='Add' />
+          </View>
+        </View>
+
+        {
+          taskLists.map(function (items, index) {
+            return  <View key={index} style={styles.TodoContainer}>
+                      <Text style={styles.TodoTask}>{ items }</Text> 
+                    </View>
+          })
+        }
+      </View>
+    </>
+  )
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
+
+export default App;
+
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'black'
   },
-});
 
-export default App;
+  inputTitle: {
+    fontSize: 20,
+    color: 'black',
+    fontWeight: '700',
+    marginBottom: 20
+  },
+
+  InputContainer: {
+    padding: 50,
+    width: '80%',
+    backgroundColor: 'white',
+    display: 'flex',
+    marginTop: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20
+  },
+
+  mainInput: {
+    borderColor: 'gray',
+    borderRadius: 5,
+    borderWidth: 1,
+    height: 40,
+    paddingHorizontal: 10,
+    width: '80%',
+    
+  },
+
+  AddInput: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    columnGap: 20
+  },
+
+  TodoContainer: {
+    width: '80%',
+    backgroundColor: 'white',
+    marginTop: 30,
+    padding: 30,
+    borderRadius: 20
+  },
+
+  TodoTask: {
+    fontSize: 20,
+    fontWeight: '700'
+  }
+})
